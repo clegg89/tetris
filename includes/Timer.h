841a94a86b5abbcd4fb6dc8d2f6c5a59f1c6234f
clegg <clegg89@gmail.com>
2016-01-07 22:39:06 -0500

    Added timer class

---
+++ includes/Timer.h
@@ -0,0 +1,15 @@
+/**
+ * @file Timer.h
+ * 
+ * @author C. Smith
+ * @date Jan 7, 2016
+ */
+
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+#include <SDL.h>
+
+#define Timer_GetTicks SDL_GetTicks
+
+#endif /* _TIMER_H_ */
