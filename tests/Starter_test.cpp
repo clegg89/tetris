7e06c4ba89a8ae302393a719403c6143c9ebd3d8
Casey Smith <clegg89@gmail.com>
2016-09-04 14:32:18 -0400

    Added starter test to get automation up and running

---
+++ tests/Starter_test.cpp
@@ -0,0 +1,26 @@
+/*
+ * @file Starter_test.cpp
+ *
+ * @brief Unit Test for the Starter class.
+ *
+ * @author C. Smith
+ */
+
+#include <CppuTest/TestHarness.h>
+#include <CppuTestExt/MockSupport.h>
+
+TEST_GROUP(starter)
+{
+  void setup()
+  {
+  }
+
+  void teardown()
+  {
+  }
+};
+
+TEST(starter, pass)
+{
+  CHECK(true);
+}
