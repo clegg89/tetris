b2f8d1c233f8718e07a04a5c3beb72fdd258af9c
Casey Smith <clegg89@gmail.com>
2016-09-10 20:30:07 -0400

    Fixed some path stuff in appveyor install scripts

--- scripts/install.bat
+++ scripts/install.bat
@@ -14,12 +14,14 @@ GOTO AFTER
 SET PATH=C:\tools\mingw%BITS%\bin;%PATH%
 RD /s /q C:\MinGW
 
+SET PROJ_DIR=%cd%
+
 IF EXIST dependencies\SFML-%SFML_VERSION% GOTO SKIP_SFML
 curl -sSL -o sfml.zip http://www.sfml-dev.org/files/SFML-%SFML_VERSION%-%SFML_COMPILER%-%BITS%-bit.zip
-unzip -q sfml.zip -d dependencies\
+unzip -q sfml.zip -d %PROJ_DIR%\dependencies\
 DEL sfml.zip
 :SKIP_SFML
-SET SFML_ROOT=dependencies\SFML-%SFML_VERSION%
+SET SFML_ROOT=%PROJ_DIR%\dependencies\SFML-%SFML_VERSION%
 
 IF EXIST dependencies\CppUTest-%CPPUTEST_VERSION% GOTO SKIP_CppUTest
 curl -sSL -o cpputest.zip https://github.com/cpputest/cpputest/releases/download/v%CPPUTEST_VERSION%/cpputest-%CPPUTEST_VERSION%.zip
@@ -29,10 +31,11 @@ CD .\cpputest-%CPPUTEST_VERSION%\cpputest_build
 SET OLD_PATH=%PATH%
 SET PATH=%PATH:C:\Program Files\Git\usr\bin;=%
 SET PATH=%PATH:C:\Program Files (x86)\Git\usr\bin;=%
-cmake -G "%GENERATOR%" -DCMAKE_INSTALL_PREFIX=..\..\dependencies\CppUTest-%CPPUTEST_VERSION% -DTESTS=OFF ..
+cmake -G "%GENERATOR%" -DCMAKE_INSTALL_PREFIX=%PROJ_DIR%\dependencies\CppUTest-%CPPUTEST_VERSION% -DTESTS=OFF ..
 SET PATH=%OLD_PATH%
 cmake --build . --target install
 CD ..\..\
 RD /s /q cpputest-%CPPUTEST_VERSION%
+CD %PROJ_DIR%
 :SKIP_CppUTest
-SET CppUTest_PATH=C:\CppUTest-%CPPUTEST_VERSION%
+SET CppUTest_PATH=%PROJ_DIR%\dependencies\CppUTest-%CPPUTEST_VERSION%
