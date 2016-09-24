0cf05defff1c5f0fad3830f761491e2b744dd207
Casey Smith <clegg89@gmail.com>
2016-09-24 10:44:50 -0400

    Refactored build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,9 +1,37 @@
-MKDIR build
-CD build
-SET OLD_PATH=%PATH%
-SET PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-SET PATH=%PATH:C:\Program Files (x86)\Git\usr\bin;=%
-cmake -G "%GENERATOR%" ..
-SET PATH=%OLD_PATH%
-cmake --build .
-cpack -G "ZIP" .
+@rem FILE: build.bat
+@rem AUTHOR: C. Smith
+@rem DESC: Build the project
+@echo off
+setlocal
+
+set self=%~nx0
+
+@rem Skip function prototypes
+goto main
+
+@rem FUNCTIONS
+
+:main
+mkdir %BUILD_DIR%
+pushd %BUILD_DIR%
+
+set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
+set PATH=%PATH:C:\Program Files (x86)\Git\usr\bin;=%
+
+echo Generate CMake files
+cmake -G "%CMAKE_GENERATOR%" .. || goto error
+
+echo Building Project
+cmake --build . || goto error
+
+echo Packaging Project
+cpack -G "ZIP" . || goto error
+
+:success
+endlocal
+echo Build successful
+goto :EOF
+
+:error
+echo %self% failed with error #%errorlevel%
+exit /b %errorlevel%
