35c3ef3ede8f9bf08e3de7b5f0a4536f979b64f0
Casey Smith <clegg89@gmail.com>
2016-10-09 12:03:30 -0400

    Squashed 'cmake/toolchains/' changes from 4723075..e86b309
    e86b309 Updates to the mingw toolchain
    
    git-subtree-dir: cmake/toolchains
    git-subtree-split: e86b3097583267dadf66164b5cf594cc0e5d5e71

--- mingw64-toolchain.cmake
+++ mingw64-toolchain.cmake
@@ -1,16 +1,28 @@
 # the name of the target operating system
-SET(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 
-# here is the target environment located
-SET(CMAKE_SYSROOT  /usr/x86_64-w64-mingw32/sys-root)
+set(MINGW_SYSROOT /usr/x86_64-w64-mingw32/sys-root)
+
+# Change default install location
+set(CMAKE_INSTALL_PREFIX ${MINGW_SYSROOT}/mingw)
+
+# Set sys-root
+set(CMAKE_SYSROOT ${MINGW_SYSROOT})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
+
+# Root path to look for externals
+set(CMAKE_FIND_ROOT_PATH ${MINGW_SYSROOT}/mingw)
+
+# Do not look for any programs in FIND_ROOT_PATH (only use system path)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# Only use includes and libraries found in FIND_ROOT_PATH
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
